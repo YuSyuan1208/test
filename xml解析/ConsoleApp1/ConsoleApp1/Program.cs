c0183e42de8c59f3d6e0845bc9a1d7de0041b9f1
YuSyuan1208 <37201761+YuSyuan1208@users.noreply.github.com>
2018-03-22 21:17:39 +0800

    Add files via upload

---
+++ xml解析/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument reader = new XmlDocument();
+            reader.Load(@"C:\Users\昱軒\Desktop\高三下\多媒體系統\xml解析\35_1.xml");
+            XmlNodeList NodeLists = reader.SelectNodes("dataList/rows");
+            for (int i = 0; i < NodeLists.Count; i++)
+            {
+                Console.WriteLine("機構名稱:" + NodeLists[i].SelectSingleNode("機構名稱").InnerText);
+                Console.WriteLine("機構狀態:" + NodeLists[i].SelectSingleNode("機構狀態").InnerText);
+                Console.WriteLine("地址:" + NodeLists[i].SelectSingleNode("地址縣市別").InnerText
+                    + NodeLists[i].SelectSingleNode("地址鄉鎮市區").InnerText
+                    + NodeLists[i].SelectSingleNode("地址街道巷弄號").InnerText);
+                Console.WriteLine("電話:" + NodeLists[i].SelectSingleNode("電話").InnerText);
+                Console.WriteLine();
+            } 
+            Console.Read();
+        }
+    }
+}
